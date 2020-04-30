8ff85a2ae92ce0883c21a4dd4d3621dc342d374e
Summer <csy13808796047@gmail.com>
2020-04-30 17:50:39 +0800

    路由分组:seedling:

---
+++ config/init.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gitlab.com/go-box/pongo2gin"
+	"gobbs/middlewares"
+	"gobbs/routes"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func Init() *gin.Engine {
+	route := gin.Default()
+	//中间件
+	//没有开启redis的时候就用cookie
+	if os.Getenv("plugin") == "1" {
+		route.Use(middlewares.SessionRedis(os.Getenv("SECRET_KEY")))
+	} else {
+		route.Use(middlewares.SessionCookie(os.Getenv("SECRET_KEY")))
+	}
+	//基础变量和加载环境变量
+	base, _ := os.Getwd()
+	_ = godotenv.Load()
+	//使用pongo2gin替换gin的模板语言,并指定静态文件目录
+	route.HTMLRender = pongo2gin.New(pongo2gin.RenderOptions{
+		TemplateDir: filepath.Join(base, "views"),
+		ContentType: "text/html; charset=utf-8",
+	})
+	// 静态文件配置
+	route.Static("/static", filepath.Join(base, "static"))
+
+	// 网站favicon
+	route.StaticFile("/favicon.ico", filepath.Join(base, "static", "favicon.ico"))
+
+	// 加载路由
+	routes.InitRouter(route.Group("web"))
+
+	if os.Getenv("plugin") == "1" {
+		// 数据库连接 （MySQL
+		models.Init()
+
+		// 缓存连接 （Redis
+		cache.Init()
+	}
+
+	// 人性化提示服务器开启成功！
+	go func() {
+		time.Sleep(1 * time.Second)
+		info := `==================== DuckyGo-MVC 启动成功！！！！！！！ ==================== `
+		fmt.Println(info)
+	}()
+
+	return route
+}
--- routes/router.go
+++ routes/router.go
@@ -2,24 +2,11 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"gobbs/pkg/setting"
-	"net/http"
+	"gobbs/controllers"
 )
 
-func InitRouter() *gin.Engine {
-	r := gin.New()
-	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
-	gin.SetMode(setting.RunMode)
-	//加载模板
-	r.LoadHTMLGlob("views/**/*")
-	//处理静态资源
-	//r.StaticFS("/asset", http.Dir("public"))
-	r.Static("/asset", "./public")
-	r.GET("/test", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Hello Gin",
-		})
-	})
-	return r
+func InitRouter(r *gin.RouterGroup) {
+
+	// 主页
+	r.GET("/", controllers.Index)
 }
